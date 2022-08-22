c871d8f2a19c987934c3d2bfb5545be41984d74a
GetApPRO <81932927+getappro@users.noreply.github.com>
2022-08-22 18:38:09 +0100

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Custom CRM - Assurance santé',
+    'version': '12.0.0.0.1',
+    'summary': 'Show your offer reports',
+    'author': 'WAHBI ACHRAF',
+    'maintainer': 'WAHBI ACHRAF',
+    'Company': 'GetapPRO',
+    'website': 'https://www.getap.pro',
+    'depends': ['crm'],
+    'license': 'LGPL-3',
+    'category': 'Sales',
+    'data':[
+        
+    ],
+    #'images': ['static/description/sale-report-banner.png'],
+    'price': 0.0,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'sequence': 5,
+}
